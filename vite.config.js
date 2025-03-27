4115837b2fb2d95b7144c7d4874897228a7be1c1
Jayakrishnan <iamjaykrish2002@gmail.com>
2025-03-27 06:06:15 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [], // Make sure axios is not in this list if you want it bundled
+    },
+  },
+});
